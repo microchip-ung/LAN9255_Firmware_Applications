24cc8a74ba0733b89d9e62533e0085500d9e5ca3
Microchip Technology <http://support.microchip.com>
2021-02-19 11:59:33 +0530

    Updated mib and image file

--- Ethernet_Apps/Demo/ETHER_APP_SIP-E53_LAN9255/firmware/src/mib.h
+++ Ethernet_Apps/Demo/ETHER_APP_SIP-E53_LAN9255/firmware/src/mib.h
@@ -1,6 +1,6 @@
 /*******************************************************************
 * FileName: mib.h
-* This file was automatically generated on Tue Feb 09 2021 11:40:46 
+* This file was automatically generated on Fri Feb 19 2021 10:21:00 
 * by mib2bib utility.
 * This file contains 'C' defines for dynamic OIDs and AgentID only.
 * Do not modify this file manually.
